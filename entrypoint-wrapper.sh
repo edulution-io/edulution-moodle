e7809925fe57a80d664fdc54750d5e8d277da975
Dennis Bölling <derdennis1012@gmail.com>
2025-04-02 13:57:20 +0200

    Create entrypoint-wrapper.sh

---
+++ entrypoint-wrapper.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y curl
+
+# Start Bitnami entrypoint in background
+/opt/bitnami/scripts/moodle/entrypoint.sh /opt/bitnami/scripts/moodle/run.sh &
+
+# Warte ein bisschen (alternativ: auf HTTP warten wie du es schon tust)
+sleep 60
+
+# Jetzt eigenes Skript ausführen
+/custom_scripts/setup.sh
+
+# Warten, bis Moodle-Prozess beendet wird
+wait
