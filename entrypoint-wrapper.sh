8a6cedac425c8e7f44e6f25d0c1d11fd52a98f7f
Lukas Spitznagel <21087610+hermanntoast@users.noreply.github.com>
2025-04-29 17:52:15 +0200

    Fix missing setup.sh in container

--- entrypoint-wrapper.sh
+++ entrypoint-wrapper.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y curl
+#apt-get update
+#apt-get install -y curl
 
 # Start Bitnami entrypoint in background
 /opt/bitnami/scripts/moodle/entrypoint.sh /opt/bitnami/scripts/moodle/run.sh &
+
+curl -fsSL https://raw.githubusercontent.com/edulution-io/edulution-moodle/refs/heads/main/setup.sh -o /setup.sh
+source /setup.sh
 
 # Warte ein bisschen (alternativ: auf HTTP warten wie du es schon tust)
 sleep 300
